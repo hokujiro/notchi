8f9d81154af0a2d70db9facd7ea818b6a4ddc003
hokujiro <p.orozcomora@gmail.com>
2025-02-01 13:07:59 +0100

    testing repo

--- app/src/main/java/com/example/madetoliveapp/presentation/TaskViewModel.kt
+++ app/src/main/java/com/example/madetoliveapp/presentation/TaskViewModel.kt
@@ -38,7 +38,7 @@ class TaskViewModel(
             }
         }
     }
-//comment to delete todo
+//testing new repo
     fun getAllTasks() {
         viewModelScope.launch {
             val result = getAllTasksUseCase.execute()
